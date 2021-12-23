7606f59dc55bd0781e879f11f5a95bcb27c4e9e1
Dima Ievlev <harajukuken1997@gmail.com>
2021-12-23 19:09:11 +0300

    исправил

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,23 +1,23 @@
-const tabs = document.getElementById('tabs1');
-const tab = document.querySelectorAll('.tab');
-const tabContent = document.querySelectorAll('.tab__content');
-
-let arrTab = Array.from(tab);
-let arrTabContent = Array.from(tabContent);
-
-let tabIndex = arrTab.findIndex(item => item.className = 'tab tab_active');
-let tabContentIndex = arrTabContent.findIndex(item => item.className = 'tab__content tab__content_active')  
-console.log(tabIndex);
-
-
-
-for (let i = 0; i < arrTab.length; i++) {
+const initTabs = (wrapperId) => {
+    const wrapper = document.querySelector(wrapperId);
+    const tabList = wrapper.querySelectorAll('.tab');
+    const tabContent = wrapper.querySelectorAll('.tab__content');
     
-    arrTab[i].addEventListener('click', function() {
-        let current = document.getElementsByClassName("tab_active");
-        current[0].className = current[0].className.replace(" tab_active", "");
-        this.className += " tab_active";
-    });
-
+    for (let i = 0; i < tabList.length; i++) {
+        tabList[i].addEventListener('click', function() {
+            let current = wrapper.getElementsByClassName("tab_active");
+            current[0].className = current[0].className.replace(" tab_active", "");
+            this.className += " tab_active";
     
+            tabContent.forEach((item, index) => {
+                if (index === i) {
+                    item.classList.add('tab__content_active');
+                } else {
+                    item.classList.remove('tab__content_active');
+                }
+            });
+        });
+    }
 }
+
+initTabs('#tabs1');
